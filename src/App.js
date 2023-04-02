3fc1b4eb768fe86126ed32e5ea8bbe886e572c6d
ONGIDI E GEOFREY <61456675+OngidiGeofrey@users.noreply.github.com>
2023-04-02 14:03:42 +0300

    export register component

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello Martin , Would you mind to come Tomorrow?
         </p>
         <a
           className="App-link"
